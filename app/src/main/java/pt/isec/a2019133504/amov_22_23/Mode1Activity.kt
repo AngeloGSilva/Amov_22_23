a9f2120815cf6e4186fe29023dad44995eccb971
MRodrigo1 <76001389+MRodrigo1@users.noreply.github.com>
2022-12-23 01:02:33 +0000

    +firebase scores + ActivityModo1

--- app/src/main/java/pt/isec/a2019133504/amov_22_23/Mode1Activity.kt
+++ app/src/main/java/pt/isec/a2019133504/amov_22_23/Mode1Activity.kt
@@ -1,12 +1,20 @@
 package pt.isec.a2019133504.amov_22_23
 
 import android.app.AlertDialog
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.NonCancellable.cancel
+import kotlinx.coroutines.channels.ticker
 import pt.isec.a2019133504.amov_22_23.Data.Cell
 import pt.isec.a2019133504.amov_22_23.Data.SinglePlayer
 import pt.isec.a2019133504.amov_22_23.View.BoardView
@@ -37,7 +45,8 @@ class Mode1Activity : AppCompatActivity(), BoardView.OnTouchListener {
     }
 
     private fun updateLevel(estado : Boolean){
-        val builder = AlertDialog.Builder(this)
+
+        /*val builder = AlertDialog.Builder(this)
         builder.setTitle("Alerta de Jogo")
         builder.setMessage("Jogo em pausa por 5 segundos!")
         builder.setCancelable(false)
@@ -47,18 +56,60 @@ class Mode1Activity : AppCompatActivity(), BoardView.OnTouchListener {
         handler.postDelayed({
             caixaAlerta.cancel()
             singlePlayer.startTimer(singlePlayer.pause)
-        }, 5000)
-    }
+        }, 5000)*/
 
+        binding.boardGame.isVisible = false
+        binding.NextLevelTimer.isVisible = true
+        binding.timertitle.isVisible = true
+        binding.timer.isVisible = false
+
+        object : CountDownTimer(1000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    binding.NextLevelTimer.text= (millisUntilFinished/1000).toString()
+                }
+                override fun onFinish() {
+                    singlePlayer.startTimer(singlePlayer.pause)
+                    binding.boardGame.isVisible = true
+                    binding.NextLevelTimer.isVisible = false
+                    binding.timer.isVisible = true
+                    binding.timertitle.isVisible = false
+                }
+            }.start()
+
+        /*val handler = Handler()
+        handler.postDelayed({
+            singlePlayer.startTimer(singlePlayer.pause)
+            binding.boardGame.isVisible = true;
+            binding.timer.isVisible = false;
+            binding.timertitle.isVisible = false;
+        }, 5000)*/
+
+    }
 
     private fun updateFim(estado : Boolean){
         //TODO melhorar mensagem de fim e guardar pontos etc
-        val builder = AlertDialog.Builder(this)
+        /*val builder = AlertDialog.Builder(this)
         builder.setTitle("Fim Jogo")
         builder.setMessage(singlePlayer.pontos.toString())
         builder.setCancelable(false)
         val caixaAlerta = builder.create()
-        caixaAlerta.show()
+        caixaAlerta.show()*/
+        binding.boardGame.isVisible = false
+        binding.NextLevelTimer.isVisible = false
+        binding.timertitle.isVisible = false
+        binding.timer.isVisible = false
+        binding.btnAddFirestore.isVisible = true
+        binding.btnMenuInicial.isVisible = true
+
+        binding.btnAddFirestore.setOnClickListener(){
+            addDataToFirestore()
+        }
+
+        binding.btnMenuInicial.setOnClickListener(){
+            /*val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)*/
+            finish()
+        }
     }
 
     private fun updateValores(valores: Pair<Double, Double>){
@@ -70,13 +121,12 @@ class Mode1Activity : AppCompatActivity(), BoardView.OnTouchListener {
     }
 
     private fun updateTimer(timer : Long){
-        binding.viewTimer.text = "seconds remaining: $timer"
+        binding.timer.text = "$timer"
     }
 
     private fun updatePontos(pontos : Int){
         binding.InfoP.text = pontos.toString()
     }
-
 
     override fun onCellTouched(row: Int, col: Int) {
         singlePlayer.updateSelectedCell(row, col)
@@ -95,6 +145,21 @@ class Mode1Activity : AppCompatActivity(), BoardView.OnTouchListener {
 
         }
         builder.show()
+    }
+
+    fun addDataToFirestore() {
+        val db = Firebase.firestore
+
+        val scores = hashMapOf(
+            "Pontuacao" to singlePlayer.pontos
+        )
+        db.collection("Top5").document("singleplayer").set(scores)
+            .addOnSuccessListener {
+                Log.i(TAG, "addDataToFirestore: Success")
+            }
+            .addOnFailureListener { e->
+                Log.i(TAG, "addDataToFirestore: ${e.message}")
+            }
     }
 }
 
