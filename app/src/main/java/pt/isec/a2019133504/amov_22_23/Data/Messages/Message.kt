e54431679c4c371bdf16b951f6acdb31ec8012e2
Leonardo Gomes <lgpy1@outlook.com>
2022-12-31 06:14:29 +0000

    test

--- app/src/main/java/pt/isec/a2019133504/amov_22_23/Data/Messages/Message.kt
+++ app/src/main/java/pt/isec/a2019133504/amov_22_23/Data/Messages/Message.kt
@@ -2,6 +2,7 @@ package pt.isec.a2019133504.amov_22_23.Data.Messages
 
 import kotlin.reflect.KClass
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -10,7 +11,7 @@ import pt.isec.a2019133504.amov_22_23.Data.Level
 import pt.isec.a2019133504.amov_22_23.Data.Player
 
 @Serializable
-class Message(val type : MessageTypes,@Transient val _payload: MessagePayload) {
+class Message(val type : MessageTypes,@Transient val _payload: MessagePayload = MessagePayload()) {
     val payload : String = Json.encodeToString(_payload)
     inline fun <reified T : MessagePayload> getPayload(clazz: KClass<T>) : T {
         return Json.decodeFromString(payload)
